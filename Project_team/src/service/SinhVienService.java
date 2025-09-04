f0e1d341b605445fe3d3a260322f03cfc0065a1f
vuonghuyy <23010714@st.phenikaa-uni.edu.vn>
2025-09-04 15:22:46 +0700

    Update SinhVienService.java

---
+++ Project_team/src/service/SinhVienService.java
@@ -0,0 +1,18 @@
+public class SinhVienService {
+    public void themSinhVien(SinhVien sinhVien) {
+
+}
+    public void suaSinhVien(int id, SinhVien sinhVienMoi) {
+
+}
+    public void xoaSinhVien(int id) {
+    
+}
+    public List<SinhVien> timKiemSinhVien(String ten) {
+ 
+}
+    public List<SinhVien> layDanhSachSinhVien() {
+   
+}
+
+}
