7565bd36dd62345fecaa1272f6ce71433645d005
LucNamTien <128336047+TienLuckyy@users.noreply.github.com>
2025-09-05 17:29:51 +0700

    Time.java

---
+++ Project_team/src/main/java/model/Time.java
@@ -0,0 +1,66 @@
+package model;
+
+public class Time {
+    int hour;
+    int minute;
+    int second;
+
+    public Time() {
+        setTime(0, 0, 0);
+    }
+
+    public Time(int h) {
+        setTime(h, 0, 0);
+    }
+
+    public Time(int h, int m) {
+        setTime(h, m, 0);
+    }
+
+    public Time(int h, int m, int s) {
+        setTime(h, m, s);
+    }
+
+    public Time setTime(int h, int m, int s) {
+        setHour(h);
+        setMinute(m);
+        setSecond(s);
+        return this;
+    }
+
+    public Time setHour(int h) {
+        hour = ((h >= 0 && h < 24) ? h : 0);
+        return this;
+    }
+
+    public Time setMinute(int m) {
+        minute = ((m >= 0 && m < 60) ? m : 0);
+        return this;
+    }
+
+    public Time setSecond(int s) {
+        second = ((s >= 0 && s < 60) ? s : 0);
+        return this;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public String toString() {
+        int displayHour = (hour == 12 || hour == 0) ? 12 : hour % 12;
+        String ampm = (hour < 12) ? "AM" : "PM";
+        String minuteStr = (minute < 10) ? "0" + minute : Integer.toString(minute);
+        String secondStr = (second < 10) ? "0" + second : Integer.toString(second);
+        
+        return displayHour + ":" + minuteStr + ":" + secondStr + " " + ampm;
+    }
+}
