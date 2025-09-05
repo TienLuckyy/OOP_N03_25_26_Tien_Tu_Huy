ca2b823780570849772caa173ff6723dff5f7f77
vuonghuyy <23010714@st.phenikaa-uni.edu.vn>
2025-09-05 17:23:07 +0700

    Delete Project_huy/src/Random.java

--- Project_huy/src/Random.java
+++
@@ -1,9 +0,0 @@
-import java.util.Random;
-
-public class Number {
-    public static void main(String[] args) {
-        Random rand = new Random();
-        float random = 1 + rand.nextFloat() * (999 - 1);
-        System.out.println(random);
-    }
-}
