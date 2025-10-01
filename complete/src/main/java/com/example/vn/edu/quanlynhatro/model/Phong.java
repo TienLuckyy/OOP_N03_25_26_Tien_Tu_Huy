94fddf51f2403fcdd9cd56fb47ffbb6c8c0deb05
Vuong Huy <23010714@st.phenikaa-uni.edu.vn>
2025-10-01 14:05:47 +0700

    Move file

---
+++ complete/src/main/java/com/example/vn/edu/quanlynhatro/model/Phong.java
@@ -0,0 +1,53 @@
+package vn.edu.quanlynhatro.model;
+import java.io.Serializable; // Thêm dòng này
+
+public class Phong implements Serializable { // Thêm "implements Serializable"
+    // Thêm dòng này để kiểm soát phiên bản, là một thói quen tốt
+    private static final long serialVersionUID = 1L;
+
+    // ... các thuộc tính còn lại giữ nguyên
+
+    // Biến thành viên private (đóng gói)
+    private String soPhong;
+    private boolean trangThai; // true = đang sử dụng, false = trống
+    private double tienDienNuoc;
+
+    // Constructor
+    public Phong(String soPhong, boolean trangThai, double tienDienNuoc) {
+        this.soPhong = soPhong;
+        this.trangThai = trangThai;
+        this.tienDienNuoc = tienDienNuoc;
+    }
+
+    // Getter & Setter
+    public String getSoPhong() {
+        return soPhong;
+    }
+
+    public void setSoPhong(String soPhong) {
+        this.soPhong = soPhong;
+    }
+
+    public boolean isTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(boolean trangThai) {
+        this.trangThai = trangThai;
+    }
+
+    public double getTienDienNuoc() {
+        return tienDienNuoc;
+    }
+
+    public void setTienDienNuoc(double tienDienNuoc) {
+        this.tienDienNuoc = tienDienNuoc;
+    }
+
+    // Hàm hiển thị thông tin
+    public void hienThiThongTin() {
+        System.out.println("Phòng: " + soPhong +
+                           " | Trạng thái: " + (trangThai ? "Đang sử dụng" : "Trống") +
+                           " | Tiền điện nước: " + tienDienNuoc);
+    }
+}
