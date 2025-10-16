a3d7eb537d6cb7637bbb05225db47f8b4dadd0e0
DNTt30 <duongngoctund2004@gmail.com>
2025-10-16 23:27:33 +0700

    Refactor PhongService methods and add transactions

--- springBoot-main/gs-serving-web-content-main/complete/src/main/java/vn/edu/quanlynhatro/service/PhongService.java
+++ springBoot-main/gs-serving-web-content-main/complete/src/main/java/vn/edu/quanlynhatro/service/PhongService.java
@@ -4,19 +4,46 @@ import vn.edu.quanlynhatro.model.Phong;
 import vn.edu.quanlynhatro.repository.PhongRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class PhongService {
-    
+
     @Autowired
     private PhongRepository phongRepository;
 
-    public Phong themPhong(Phong phong) { return phongRepository.save(phong); }
-    public Phong suaPhong(Phong phong) { return phongRepository.save(phong); } 
-    public void xoaPhong(String soPhong) { phongRepository.deleteById(soPhong); }
-    public Optional<Phong> timKiemTheoSoPhong(String soPhong) { return phongRepository.findById(soPhong); }
-    public List<Phong> timKiemPhongTrong() { return phongRepository.findByTrangThai(false); }
-    public List<Phong> getAllPhong() { return phongRepository.findAll(); }
+    public List<Phong> getAllPhong() {
+        return phongRepository.findAll();
+    }
+
+    public Optional<Phong> timKiemTheoSoPhong(String soPhong) {
+        return phongRepository.findById(soPhong);
+    }
+
+    public List<Phong> timKiemPhongTrong() {
+        return phongRepository.findPhongConCho();
+    }
+
+    public List<Phong> getPhongTheoTrangThai(boolean trangThai) {
+        return phongRepository.findByTrangThai(trangThai);
+    }
+
+    public List<Phong> getPhongTheoToa(String toa) {
+        return phongRepository.findByToa(toa);
+    }
+
+    public void themPhong(Phong phong) {
+        phongRepository.save(phong);
+    }
+
+    public void suaPhong(Phong phong) {
+        phongRepository.save(phong);
+    }
+
+    public void xoaPhong(String soPhong) {
+        phongRepository.deleteById(soPhong);
+    }
 }
