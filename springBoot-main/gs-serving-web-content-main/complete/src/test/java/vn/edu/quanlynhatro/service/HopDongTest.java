ea45ec2617020b0364678c1d6d6a76a9f529a925
TienLuckyy <duongngoctund2004@gmail.com>
2025-10-17 18:03:24 +0700

    done 1/4 thiếu File.txt

--- springBoot-main/gs-serving-web-content-main/complete/src/test/java/vn/edu/quanlynhatro/service/HopDongTest.java
+++ springBoot-main/gs-serving-web-content-main/complete/src/test/java/vn/edu/quanlynhatro/service/HopDongTest.java
@@ -1,33 +1,33 @@
-package vn.edu.quanlynhatro.service;
+// package vn.edu.quanlynhatro.service;
 
-import vn.edu.quanlynhatro.model.HopDong;
+// import vn.edu.quanlynhatro.model.HopDong;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
+// import java.util.ArrayList;
+// import java.util.Calendar;
+// import java.util.Date;
 
-public class HopDongTest{
-    public static void main(String[]args ){
-        Calendar cal = Calendar.getInstance();
+// public class HopDongTest{
+//     public static void main(String[]args ){
+//         Calendar cal = Calendar.getInstance();
 
-        cal.set(2025,Calendar.SEPTEMBER,1);
-        Date ngayBatDau1 = cal.getTime();
-        cal.set(2026,Calendar.AUGUST,31);
-        Date ngayKetThuc1 = cal.getTime();
+//         cal.set(2025,Calendar.SEPTEMBER,1);
+//         Date ngayBatDau1 = cal.getTime();
+//         cal.set(2026,Calendar.AUGUST,31);
+//         Date ngayKetThuc1 = cal.getTime();
 
-        cal.set(2025,Calendar.JANUARY,15);
-        Date ngayBatDau2 = cal.getTime();
-        cal.set(2027,Calendar.JULY,15);
-        Date ngayKetThuc2 = cal.getTime();
+//         cal.set(2025,Calendar.JANUARY,15);
+//         Date ngayBatDau2 = cal.getTime();
+//         cal.set(2027,Calendar.JULY,15);
+//         Date ngayKetThuc2 = cal.getTime();
 
-        HopDong hd1 = new HopDong("HD001","SV001","P101", ngayBatDau1,ngayKetThuc1,2000000,"Dang hieu luc");
-        HopDong hd2 = new HopDong("HD002","SV002","P102", ngayBatDau2,ngayKetThuc2,2500000,"Da het han");
+//         HopDong hd1 = new HopDong("HD001","SV001","P101", ngayBatDau1,ngayKetThuc1,2000000,"Dang hieu luc");
+//         HopDong hd2 = new HopDong("HD002","SV002","P102", ngayBatDau2,ngayKetThuc2,2500000,"Da het han");
         
-        ArrayList<HopDong> listHD=new ArrayList<>();
-        listHD.add(hd1);
-        listHD.add(hd2);
+//         ArrayList<HopDong> listHD=new ArrayList<>();
+//         listHD.add(hd1);
+//         listHD.add(hd2);
     
-        for (HopDong hd : listHD){
-            System.out.println(hd.toString());
-        }}
-}
+//         for (HopDong hd : listHD){
+//             System.out.println(hd.toString());
+//         }}
+// }
